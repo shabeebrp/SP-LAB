5a04cc2107f81282a88a010fc8e3350e57caa605
shabeebrp <shabeebrp@gmail.com>
2016-05-01 10:34:59 +0530

    Create lex.cpp

---
+++ lex.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<vector>
+#include<fstream>
+#include<algorithm>
+using namespace std;
+int line_no = 1;
+
+vector<string> delims = { "{", "}", "[", "]", ",", ";", "." , "(", ")" };
+vector<string> operators = { "+", "-", "*", "/" };
+vector<string> relOps = { "<", ">", "!" };
+vector<string> logOps = { "&", "|" };
+vector<string> keyword = {
+	"int", "float", "double", "char", "void",
+	"while", "for", "do", "if", "return"
+};
+bool isDelim(string token) {
+	return(find(delims.begin(), delims.end(), token) != delims.end());
+}
+bool isOperator(string token) {
+	return(find(operators.begin(), operators.end(), token) != operators.end());
+}
+bool isRelOp(string token) {
+	return(find(relOps.begin(), relOps.end(), token) != relOps.end());
+}
+bool isLogOp(string token) {
+	return(find(logOps.begin(), logOps.end(), token) != logOps.end());
+}
+bool isKeyword(string token) {
+	return(find(keyword.begin(), keyword.end(), token) != keyword.end());
+}
+bool isNumber(string token) {
+	for (auto i = token.begin(); i != token.end(); i++)
+		if (!isdigit(*i))
+			return false;
+	return true;
+}
+void print(string token) {
+	if (isDelim(token))
+		cout << "Delim\t:\t" << token << endl;
+	else if (isOperator(token)) 
+		cout << "Arithm\t:\t" << token << endl;
+	else if (isRelOp(token))
+		cout << "Relational\t:\t" << token << endl;
+	else if (isLogOp(token))
+		cout << "Logical\t:\t" << token << endl;
+	else if (isKeyword(token))
+		cout << "keyword\t:\t" << token << endl;
+	else if (token == "=")
+		cout << "Assign\t:\t" << token << endl;
+	else if (isNumber(token))
+		cout << "Number\t:\t" << token << endl;
+	else if (isalpha(token[0]))
+		cout << "ident\t:\t" << token << endl;
+	else
+		cout << "Invalid\t:\t" << token << endl;
+
+}
+void analyze(string token) {
+	//cout << token << endl;
+	string current = "";
+	for (int i = 0; i < token.size(); i++)
+		if (isalnum(token[i]))
+			current += string(1, token[i]);
+		else {
+			if (current.size() > 0) {
+				print(current);
+				current = "";
+			}
+			print(string(1, token[i]));
+		}
+	if (current.size() > 0)
+		print(current);
+}
+
+int main() {
+	ifstream fin("lex.txt");
+	string line;
+	while (getline(fin, line)) {
+		stringstream ss(line);
+		string token;
+		cout << "Line " << line_no << "\n-------------------\n";
+		while (getline(ss, token, ' '))
+			analyze(token);
+		line_no++;
+		cout << endl;
+	}
+	return 0;
+}
